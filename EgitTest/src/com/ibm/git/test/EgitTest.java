1947d1b2e09169777a63b8d0135dacec60de590b
yadoble <yadoble@gmail.com>
2016-03-17 07:45:00 -0600

    This is just a test

---
+++ EgitTest/src/com/ibm/git/test/EgitTest.java
@@ -0,0 +1,15 @@
+package com.ibm.git.test;
+
+public class EgitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is only a test for eGit");
+		
+		System.out.println("one more change to be added to a different branch");
+	}
+
+}
