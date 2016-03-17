9260b98cc3b5a4c51119b352cf88be4391a89239
yadoble <yadoble@gmail.com>
2016-03-17 09:50:19 -0600

    This is the example to add one file to Git

---
+++ EgitTest/src/com/ibm/git/test/EgitOneMore.java
@@ -0,0 +1,13 @@
+package com.ibm.git.test;
+
+public class EgitOneMore {
+	
+	public void EgitOneMore(){
+		
+	}
+	
+	public void EgitOneMore(String uno){
+		System.out.println("This is the string: " + uno);
+	}
+
+}
